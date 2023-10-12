76ffa6bea6c14538d5044091115c6860fdeead63
ArpitBlagan <114584343+ArpitBlagan@users.noreply.github.com>
2023-10-12 20:54:28 +0530

    Update Validate.js

--- middelware/Validate.js
+++ middelware/Validate.js
@@ -2,7 +2,7 @@ const asyncHandler=require('express-async-handler');
 const jwt=require('jsonwebtoken');
 exports.Validate=asyncHandler(async(req,res,next)=>{
     //using the cookie that we send while user login in our app
-    const token=req.cookies.jwt;console.log("jwt",token);
+    const token=req.cookies.jwt;
         jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
             if(err){
                 return res.status(200).json({

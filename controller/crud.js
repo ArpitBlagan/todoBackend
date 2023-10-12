220439024bf5b0647e65ca5041ba949d6bf95031
ArpitBlagan <114584343+ArpitBlagan@users.noreply.github.com>
2023-10-12 20:54:12 +0530

    Update crud.js

--- controller/crud.js
+++ controller/crud.js
@@ -24,6 +24,7 @@ exports.del=asyncHandler(async(req,res)=>{
 })
 exports.add=asyncHandler(async(req,res)=>{
     const {title,description,priority}=req.body;
+    console.log(req.user);
     const id=req.user.id;
     const val=await tDB.create({title,description,priority,user_id:req.user.id});
     if(val){
